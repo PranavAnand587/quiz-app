96b8b8abc5e77f7c0b43ab19b36d1860054bf61b
PranavAnand587 <pranav20172017@gmail.com>
2020-04-22 12:26:47 +0530

    Display Options

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from "react"
 
 function Quiz() {
 
-    const [ data,setData ] = useState("none")
-    const [ questionNo,setQuestionNo ] = useState(0)
-
-    let options=[]
+    const [data, setData] = useState("none")
+    const [questionNo, setQuestionNo] = useState(1)
+    const [options, setOptions] = useState([])
 
     function shuffle(array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
@@ -18,33 +17,33 @@ function Quiz() {
             temporaryValue = array[currentIndex];
             array[currentIndex] = array[randomIndex];
             array[randomIndex] = temporaryValue;
-  }
+        }
 
-  return array;
-}
+        return array;
+    }
 
     useEffect(() => {
-    async function fetchData(){
-        const res = await fetch("https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple")
-        const _data = await res.json()
-        console.table(_data.results)
-        setData(_data.results[0])
-        const options = _data.results[0].incorrect_answers
-        options.push(_data.results[0].correct_answer)
-        shuffle(options)
-        console.log(options)
-    }
+        async function fetchData() {
+            const res = await fetch("https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple")
+            const _data = await res.json()
+            console.table(_data.results)
+            setData(_data.results[0])
+            const options = _data.results[0].incorrect_answers
+            options.push(_data.results[0].correct_answer)
+            shuffle(options)
+            setOptions(options)
+        }
         fetchData()
-    },[questionNo])
+    }, [questionNo])
 
-    return(
+    return (
         <div>
-            <p>{data.question}</p>
+            <p>{questionNo}) {data.question}</p>
             <input type="radio" name="choice" id="val1" /><label for="val1">{options[0]}</label>
             <input type="radio" name="choice" id="val2" /><label for="val2">{options[1]}</label>
             <input type="radio" name="choice" id="val3" /><label for="val3">{options[2]}</label>
             <input type="radio" name="choice" id="val4" /><label for="val4">{options[3]}</label>
-            <button onClick={() => setQuestionNo(questionNo+1)}>Next</button>
+            <button onClick={() => setQuestionNo(questionNo + 1)}>Next</button>
         </div>
     )
 }
