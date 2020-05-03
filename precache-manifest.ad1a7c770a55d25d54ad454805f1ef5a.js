26cf08fd3f14e6a59e3b378ca967f2a0f29cc119
PranavAnand587 <pranav20172017@gmail.com>
2020-05-03 20:33:11 +0530

    Updates

---
+++ precache-manifest.ad1a7c770a55d25d54ad454805f1ef5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8395168d55af377c6d350d693f2f68d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e853d28917bcd2362bb",
+    "url": "/static/css/main.9273dec3.chunk.css"
+  },
+  {
+    "revision": "9fc2942c725a73bff012",
+    "url": "/static/js/2.417c4d00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.417c4d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e853d28917bcd2362bb",
+    "url": "/static/js/main.2f192fd6.chunk.js"
+  },
+  {
+    "revision": "f1e317db8882717585b5",
+    "url": "/static/js/runtime-main.6d4ec069.js"
+  }
+]);
